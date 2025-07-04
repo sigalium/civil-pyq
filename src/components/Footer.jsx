7e579fc1887dfe2bf68a3e6bc3ab89482cd47aea
Priyangkam Bhuyan <140977316+sigalium@users.noreply.github.com>
2025-07-04 17:56:48 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <div className="footer-container">
         <div className="footer-links">
           <h4>GCU Links</h4>
-          <a href="https://gcuniversity.ac.in/academics/" target="_blank" rel="noopener noreferrer">Academics</a>         
+          <a href="https://gcuniversity.ac.in/school-of-engineering-and-technology/" target="_blank" rel="noopener noreferrer">Academics</a>         
           <a href="https://gcuniversity.ac.in/" target="_blank" rel="noopener noreferrer">GCU Website</a> 
           <a href="https://gcu-dl.bsmlib.com/" target="_blank" rel="noopener noreferrer">Digital Library</a>  
           <a href="https://gcuniversity.ac.in/contact/" target="_blank" rel="noopener noreferrer">Contact GCU</a>
