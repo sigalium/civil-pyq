8d6be8ef39a9ea26c3ad407a1c85c6c083647278
Priyangkam Bhuyan <140977316+sigalium@users.noreply.github.com>
2025-07-18 18:50:03 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -22,6 +22,7 @@ const Footer = () => {
         <div className="footer-contact">
           <h4>Contact</h4>
           <a href="https://priyangkam.netlify.app" target="_blank" rel="noopener noreferrer">Website</a>
+          <a href="https://www.instagram.com/priyangkamb" target="_blank" rel="noopener noreferrer">Instagram</a>
           <a href="https://github.com/sigalium" target="_blank" rel="noopener noreferrer">GitHub</a>
           <a href="mailto:contact@civilpyq.com">Email</a>     
         </div>
